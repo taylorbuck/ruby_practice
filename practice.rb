bc13c365ec9f2813e86876e384b093a72c49b443
EC2 Default User <ec2-user@ip-172-31-17-248.us-west-2.compute.internal>
2018-04-14 19:26:25 +0000

    added to test

--- practice.rb
+++ practice.rb
@@ -1,7 +1,28 @@
-users = 49
-
-if (users >50)
-    puts "Hey, we hit 50 users!"
-else
-    puts "Haven't hit 50 users yet..."
+#for reference
+def sorted?(array)
+	array.each_with_index do |value, index|
+		if (index < (array.length - 1)) && (array[index] > array[index + 1])
+			return false
+		end
+	end
+	true
 end
+
+
+def bubble_sort(array)
+	sorted_array = array
+
+	until sorted?(sorted_array)
+		helper_array = sorted_array
+		helper_array.each_with_index do |value, index|
+			if (index < (helper_array.length - 1)) && (sorted_array[index] > sorted_array[index + 1])
+				helper_var = sorted_array[index]
+				sorted_array[index] = sorted_array[index + 1]
+				sorted_array[index + 1] = helper_var
+			end
+		end
+	end
+	puts sorted_array.inspect
+end
+
+bubble_sort([4,3,78,2,0,2])
